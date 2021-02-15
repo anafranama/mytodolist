92f8704fac71486b66f502db4aa7babb082689c1
anafranama <anafranama@gmail.com>
2021-02-15 22:47:36 +0000

    primercommit

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,65 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
+import shortid from "shortid";
 
 //create your first component
 export function Home() {
+	const [tarea, setTarea] = useState("");
+	const [arrayTareas, setArrayTareas] = useState([]);
+
+	let tareasPendientes = arrayTareas.length;
+
+	const eliminar = idUnico => {
+		let itemEliminar = arrayTareas.filter(item => {
+			return item.id !== idUnico;
+		});
+		setArrayTareas(itemEliminar);
+	};
+
+	const agregarTarea = e => {
+		e.preventDefault();
+		setArrayTareas([
+			...arrayTareas,
+			{
+				id: shortid.generate(),
+				nombreTarea: tarea
+			}
+		]);
+		setTarea("");
+	};
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="app">
+			<header className="header">
+				<h1>Todos</h1>
+				<form onSubmit={agregarTarea}>
+					<input
+						placeholder="¿Qué necesita hacer?"
+						onChange={e => setTarea(e.target.value)}
+						value={tarea}
+					/>
+				</form>
+			</header>
+			<section>
+				{tareasPendientes === 0 ? (
+					<p>No hay tareas pendientes</p>
+				) : (
+					arrayTareas.map(item => (
+						<div className="lista" key={item.id}>
+							<p>
+								{item.nombreTarea}{" "}
+								<i
+									onClick={() => {
+										eliminar(item.id);
+									}}
+									className="fas fa-trash"></i>
+							</p>
+						</div>
+					))
+				)}
+			</section>
+			<footer className="footer">
+				<p>Quedan {tareasPendientes} tareas pendientes.</p>
+			</footer>
 		</div>
 	);
 }
